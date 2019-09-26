1fdc5bf4fb923c4dbb9cb2f61c5502e5b98479bb
HectorCo <hcompanyortega@gmail.com>
2019-09-26 11:59:00 +0200

    agrega una coleccion con los resultados

--- index.js
+++ index.js
@@ -5,8 +5,28 @@ db.frases.aggregate([
   { $project: { frase: { $split: ["$frase", " "] }, cantidad: {$add : 1} } },
   { $unwind: "$frase" },
   { $group: { _id: { palabras: "$frase" }, cantidad_total: { $sum: "$cantidad" } } },
-  { $sort: { cantidad_total: -1 } }
+  { $sort: { cantidad_total: -1 } },
+  {$out: "palabras-agg"}
 
 ]);
+
+
+
+/* USANDO LA SIGUIENTE BDD
+
+use running
+db.sesiones.insert({nombre:"Bertoldo", mes:"Marzo", distKm:6, tiempoMin:42})
+db.sesiones.insert({nombre:"Herminia", mes:"Marzo", distKm:10, tiempoMin:60})
+db.sesiones.insert({nombre:"Bertoldo", mes:"Marzo", distKm:2, tiempoMin:12})
+db.sesiones.insert({nombre:"Herminia", mes:"Marzo", distKm:10, tiempoMin:61})
+db.sesiones.insert({nombre:"Bertoldo", mes:"Abril", distKm:5, tiempoMin:33})
+db.sesiones.insert({nombre:"Herminia", mes:"Abril", distKm:42, tiempoMin:285})
+db.sesiones.insert({nombre:"Aniceto", mes:"Abril", distKm:5, tiempoMin:33})
+
+
+
+
+
+*/
 
 
