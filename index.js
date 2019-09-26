2f5fe810f1a96838a38bfb3c323de82165d700cc
HectorCo <hcompanyortega@gmail.com>
2019-09-26 11:41:28 +0200

    cuenta palabras

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // contar palabras
 
+
 db.frases.aggregate([
   { $project: { frase: { $split: ["$frase", " "] }, cantidad: {$add : 1} } },
   { $unwind: "$frase" },
   { $group: { _id: { palabras: "$frase" }, cantidad_total: { $sum: "$cantidad" } } },
   { $sort: { cantidad_total: -1 } }
+
 ]);
+
+
